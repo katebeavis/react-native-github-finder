77354b60fdc7b8e3681407dc7320e83d46aba585
Kate Beavis <katebeavis84@gmail.com>
2020-04-13 14:15:56 +0100

    Add text input

--- App/Components/Home/Home.tsx
+++ App/Components/Home/Home.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicator,
+} from 'react-native';
 
 import styles from './Home.styles';
 
 const Home = () => {
+  const [username, setUsername] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [error, setError] = useState<boolean>(false);
+
+  const handleSubmit = () => {
+    alert(username);
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.title}>Hello world</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Text style={styles.title}>Search for a Github user</Text>
+          <TextInput
+            style={styles.searchInput}
+            value={username}
+            onChangeText={(input) => setUsername(input)}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={handleSubmit}
+            underlayColor={'white'}
+            accessibilityLabel={'Submit'}
+          >
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableHighlight>
+        </>
+      )}
     </View>
   );
 };
